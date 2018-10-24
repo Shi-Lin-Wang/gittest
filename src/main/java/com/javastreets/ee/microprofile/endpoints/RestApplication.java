02ae9ffc6fb58eb2241e306253c7c5d7ef656cee
SHILIN <asd53172@gmail.com>
2018-10-24 09:58:29 +0800

    v1

---
+++ src/main/java/com/javastreets/ee/microprofile/endpoints/RestApplication.java
@@ -0,0 +1,9 @@
+package com.javastreets.ee.microprofile.endpoints;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
